5112175608f98214c9730336f6cf8a4c35001ed2
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-06-26 15:10:38 +0200

    Styled cards in selection screen

--- components/CharacterSelect/CharacterSelect.js
+++ components/CharacterSelect/CharacterSelect.js
@@ -4,7 +4,39 @@ import {
     CharacterSelectWrapper,
 } from './CharacterSelect.styled';
 
-const characters = ['Bard', 'Anivia', 'TF', 'Brand'];
+const characters = [
+    {
+        id: 1,
+        image: '/images/bard.webp',
+        name: 'Bard',
+    },
+    {
+        id: 2,
+        image: 'images/anivia.webp',
+        name: 'Anivia',
+    },
+    {
+        id: 3,
+        image: 'images/twistedfate.webp',
+        name: 'TF',
+    },
+    {
+        id: 4,
+        image: 'images/brand.webp',
+        name: 'Brand',
+    },
+];
+
+const checkIfFirst = (character, array) => {
+    const _firstIndex = 0;
+
+    return array.indexOf(character) === _firstIndex;
+};
+
+const checkIfLast = (character, array) => {
+    const _lastIndex = array.length - 1;
+    return array.indexOf(character) === _lastIndex;
+};
 
 export default function CharacterSelect() {
     const [previewCharacterId, setPreviewCharacterId] = useState(null);
@@ -14,8 +46,15 @@ export default function CharacterSelect() {
     // );
     return (
         <CharacterSelectWrapper>
-            {characters.map((name) => (
-                <CharacterCard key={name}>{name}</CharacterCard>
+            {characters.map((character, _, array) => (
+                <CharacterCard
+                    key={character.id}
+                    image={character.image}
+                    isFirst={checkIfFirst(character, array)}
+                    isLast={checkIfLast(character, array)}
+                >
+                    {character.name}
+                </CharacterCard>
             ))}
         </CharacterSelectWrapper>
     );
--- components/CharacterSelect/CharacterSelect.styled.js
+++ components/CharacterSelect/CharacterSelect.styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-const transitionDuration = '315ms';
+const transitionDuration = '215ms';
+const transitionTimingFunction = 'cubic-bezier(0.22, 0.61, 0.36, 1)';
+const translateLength = '80px';
 
 export const CharacterSelectWrapper = styled.div`
     padding: 10px;
@@ -15,12 +17,27 @@ export const CharacterCard = styled.button`
     width: 300px;
     background-color: grey;
 
+    background-image: ${(props) => `url(${props.image})`};
+    background-position: right;
+    background-size: cover;
+
+    filter: blur(1px) grayscale(80%);
+
     &:hover {
         cursor: pointer;
         background-color: black;
         color: white;
-        transform: translate(10px, 15px);
+        background-position: center;
+        filter: unset;
+
+        transform: ${(props) =>
+            `
+            ${props.isFirst ? `translateX(${translateLength})` : ''} 
+            ${props.isLast ? `translateX(-${translateLength})` : ''}
+            translateY(${translateLength})
+            scale(1.4)
+            `};
     }
 
-    transition: all ${transitionDuration};
+    transition: all ${transitionDuration} ${transitionTimingFunction};
 `;
