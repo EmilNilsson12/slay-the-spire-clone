4111ea1e7d38cadcb1bb7a08a19bfb0deaccb733
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-07-04 13:11:05 +0200

    Refactor character select noises

--- components/CharacterSelect/CharacterSelect.js
+++ components/CharacterSelect/CharacterSelect.js
@@ -15,31 +15,37 @@ import {
 const characters = [
     {
         id: 1,
-        image: '/images/bard.webp',
         name: 'Bard',
+        image: '/images/bard.webp',
         description: 'Bard uses meeps and portals to outsmart his enemies 🤔',
     },
     {
         id: 2,
-        image: 'images/anivia.webp',
         name: 'Anivia',
+        image: 'images/anivia.webp',
         description:
             'Anivia uses ice walls and blizzards to control her surroundings 🥶',
     },
     {
         id: 3,
+        name: 'Twisted Fate',
         image: 'images/twistedfate.webp',
-        name: 'TF',
         description:
             'Twisted fate uses cards and tricks to decieve his enemies 😈',
     },
     {
         id: 4,
-        image: 'images/brand.webp',
         name: 'Brand',
+        image: 'images/brand.webp',
         description: 'Brand just wants to watch the world burn 🥵',
     },
 ];
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 const checkIfFirst = (character, array) => {
     const _firstIndex = 0;
@@ -59,88 +65,80 @@ export default function CharacterSelect({ callBackFunc }) {
         useState(false);
     const [bioVisible, setBioVisible] = useState(false);
 
-    const handleCharacterCardClick = (clickedCharacterId) => {
-        setBioVisible(true);
-
-        if (clickedCharacterId !== previewCharacterId) {
-            myToggleHook(setBioAnimationPlaying, 500);
-        }
-
-        myToggleHook(setBackgroundAnimationIsPlaying, 500);
-
-        setPreviewCharacterId(clickedCharacterId);
-
-        backgroundMusic.current.play();
-
-        characterselectedsoundRef.current.play();
-        switch (clickedCharacterId) {
-            // Bard sounds
-            case 1:
-                Math.random() > 0.66
-                    ? partyhornRef.current.play()
-                    : Math.random() > 0.33
-                    ? obnoxiouswhistleRef.current.play()
-                    : bahhumbugRef.current.play();
-                break;
-
-            // Anivia sounds
-            case 2:
-                snowstormRef.current.play();
-                Math.random() > 0.5
-                    ? cawcawshortRef.current.play()
-                    : cawcawlongRef.current.play();
-                babychicktweetRef.current.play();
-                break;
-
-            // Twisted fate sounds
-            case 3:
-                Math.random() > 0.5
-                    ? hahahalaughRef.current.play()
-                    : slotmachinewinRef.current.play();
-
-                break;
-
-            // Brand sounds
-            case 4:
-                Math.random() > 0.5
-                    ? evillaughRef.current.play()
-                    : fireRef.current.play();
-
-                break;
-
-            default:
-                growlRef.current.play();
-                break;
-        }
-    };
-
-    const embark = () => {
-        callBackFunc('main-menu-map');
-    };
-
-    const previewCharacter = characters.find(
-        (character) => character.id === previewCharacterId
-    );
-
     const backgroundMusic = React.createRef();
-    const growlRef = React.createRef();
-    const snowstormRef = React.createRef();
     const cawcawshortRef = React.createRef();
     const cawcawlongRef = React.createRef();
     const fireRef = React.createRef();
     const fire2Ref = React.createRef();
     const babychicktweetRef = React.createRef();
     const bahhumbugRef = React.createRef();
-    const bensoundsavehumanityRef = React.createRef();
     const cashmachinekachingRef = React.createRef();
     const characterselectedsoundRef = React.createRef();
     const evillaughRef = React.createRef();
     const glassbreaksRef = React.createRef();
     const hahahalaughRef = React.createRef();
     const obnoxiouswhistleRef = React.createRef();
-    const slapRef = React.createRef();
     const slotmachinewinRef = React.createRef();
     const partyhornRef = React.createRef();
+
+    const soundfxRefs = [
+        {
+            character: 1,
+            fx: [bahhumbugRef, partyhornRef, obnoxiouswhistleRef],
+        },
+        {
+            character: 2,
+            fx: [cawcawshortRef, cawcawlongRef, babychicktweetRef],
+        },
+        {
+            character: 3,
+            fx: [hahahalaughRef, slotmachinewinRef, cashmachinekachingRef],
+        },
+        {
+            character: 4,
+            fx: [evillaughRef, fireRef, fire2Ref],
+        },
+    ];
+
+    // Global let bc its modified in both click event
+    let characterNoise;
+
+    const handleCharacterCardClick = (clickedCharacterId) => {
+        setBioVisible(true);
+
+        // Only animate character bio if new character clicked
+        if (clickedCharacterId !== previewCharacterId) {
+            myToggleHook(setBioAnimationPlaying, 500);
+        }
+
+        // Always animate background
+        myToggleHook(setBackgroundAnimationIsPlaying, 500);
+
+        setPreviewCharacterId(clickedCharacterId);
+
+        characterNoise = soundfxRefs.find(
+            (obj) => obj.character === clickedCharacterId
+        ).fx[getRandomInt(0, 3)];
+
+        backgroundMusic.current.play();
+        characterNoise.current.play();
+    };
+
+    const embark = () => {
+        backgroundMusic.current.pause();
+
+        setTimeout(() => {
+            glassbreaksRef.current.play();
+        }, 100);
+
+        setTimeout(() => {
+            callBackFunc('main-menu-map');
+        }, 1200);
+    };
+
+    const previewCharacter = characters.find(
+        (character) => character.id === previewCharacterId
+    );
 
     return (
         <CharacterSelectWrapper>
@@ -178,36 +176,50 @@ export default function CharacterSelect({ callBackFunc }) {
                     Select this character
                 </EmbarkButton>
             )}
+            {/* ALWAYS PLAYING */}
             <audio loop src="music/extremeaction.mp3" ref={backgroundMusic} />
+            {/* ALWAYS PLAYING */}
 
-            <audio src="music/growl.wav" ref={growlRef} />
-            <audio src="music/snowstorm.wav" ref={snowstormRef} />
-            <audio src="music/crowcawshort.wav" ref={cawcawshortRef} />
-            <audio src="music/crowcawlong.wav" ref={cawcawlongRef} />
-            <audio src="music/fire.wav" ref={fireRef} />
-            <audio src="music/fire2.wav" ref={fire2Ref} />
-            <audio src="music/babychicktweet.wav" ref={babychicktweetRef} />
-
-            <audio src="music/bahhumbug.wav" ref={bahhumbugRef} />
+            {/* CHARACTER ICON CLICKED */}
             <audio
-                src="music/bensoundsavehumanity.mp3"
-                ref={bensoundsavehumanityRef}
-            />
-            <audio
-                src="music/cashmachinekaching.wav"
-                ref={cashmachinekachingRef}
-            />
-            <audio
-                src="music/characterselectedsound.wav"
+                src="soundfx/characterselectedsound.wav"
                 ref={characterselectedsoundRef}
             />
-            <audio src="music/evillaugh.wav" ref={evillaughRef} />
-            <audio src="music/glassbreaks.wav" ref={glassbreaksRef} />
-            <audio src="music/hahahalaugh.wav" ref={hahahalaughRef} />
-            <audio src="music/obnoxiouswhistle.wav" ref={obnoxiouswhistleRef} />
-            <audio src="music/slap.wav" ref={slapRef} />
-            <audio src="music/slotmachinewin.wav" ref={slotmachinewinRef} />
-            <audio src="music/partyhorn.wav" ref={partyhornRef} />
+            {/* CHARACTER ICON CLICKED */}
+
+            {/* EMBARK CLICKED */}
+            <audio src="soundfx/glassbreaks.wav" ref={glassbreaksRef} />
+            {/* EMBARK CLICKED */}
+
+            {/* BARD NOISED */}
+            <audio src="soundfx/bahhumbug.wav" ref={bahhumbugRef} />
+            <audio src="soundfx/partyhorn.wav" ref={partyhornRef} />
+            <audio
+                src="soundfx/obnoxiouswhistle.wav"
+                ref={obnoxiouswhistleRef}
+            />
+            {/* BARD NOISED */}
+
+            {/* ANIVIA NOISED */}
+            <audio src="soundfx/crowcawshort.wav" ref={cawcawshortRef} />
+            <audio src="soundfx/crowcawlong.wav" ref={cawcawlongRef} />
+            <audio src="soundfx/babychicktweet.wav" ref={babychicktweetRef} />
+            {/* ANIVIA NOISED */}
+
+            {/* TWISTED FATE NOISED */}
+            <audio src="soundfx/hahahalaugh.wav" ref={hahahalaughRef} />
+            <audio src="soundfx/slotmachinewin.wav" ref={slotmachinewinRef} />
+            <audio
+                src="soundfx/cashmachinekaching.wav"
+                ref={cashmachinekachingRef}
+            />
+            {/* TWISTED FATE NOISED */}
+
+            {/* BRAND NOISED */}
+            <audio src="soundfx/evillaugh.wav" ref={evillaughRef} />
+            <audio src="soundfx/fire.wav" ref={fireRef} />
+            <audio src="soundfx/fire2.wav" ref={fire2Ref} />
+            {/* BRAND NOISED */}
         </CharacterSelectWrapper>
     );
 }
