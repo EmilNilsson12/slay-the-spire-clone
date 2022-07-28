31e8cc9f2185c8dc67c2a14eeb9e4db9a5f4f3d4
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-07-28 15:15:42 +0200

    Change level icons

--- components/MapLevelSelect/MapLevelSelect.js
+++ components/MapLevelSelect/MapLevelSelect.js
@@ -4,12 +4,24 @@ import {
     LevelIcon,
     LevelIconWrapper,
     MapGrid,
+    MapLegend,
     MapLevelSelectWrapper,
     RowInMap,
 } from './MapLevelSelect.styled';
 
+const levelTypes = [
+    { name: 'Encounter', color: 'orange' },
+    { name: 'Shop', color: 'gold' },
+    { name: 'Boss', color: 'red' },
+    { name: 'Mystery', color: 'white' },
+];
+
 export default function MapLevelSelect({ prevScreen, callBackFunc }) {
     const [currentRow, setCurrentRow] = useState(1);
+
+    const [generatedLevels, setGeneratedLevels] = useState(null);
+
+    const [highlightedLevelType, setHighlightedLevelType] = useState('Mystery');
 
     const handleClick = ({ isOnCurrentRow, levelNumber }) => {
         console.log(`levelNumber "${levelNumber}" clicked`);
@@ -47,17 +59,21 @@ export default function MapLevelSelect({ prevScreen, callBackFunc }) {
         const levelsInThisRow = [];
 
         for (let i = 1; i <= levelsInRow; i++) {
+            const levelType = levelTypes[getRandomNum(0, 3)];
+
             levelsInThisRow.push(
                 <LevelIconWrapper>
                     <LevelIcon
                         key={i}
+                        levelType={levelType}
+                        isHighlighted={levelType.name === highlightedLevelType}
                         isCurrentRow={isOnCurrentRow}
                         isOldRow={isOldRow}
                         onClick={() =>
                             handleClick({ isOnCurrentRow, levelNumber })
                         }
                     >
-                        Level {levelNumber}
+                        {levelType.name}: <br /> Level {levelNumber}
                     </LevelIcon>
                 </LevelIconWrapper>
             );
@@ -87,7 +103,25 @@ export default function MapLevelSelect({ prevScreen, callBackFunc }) {
         <>
             <BackBtn prevScreen={prevScreen} cb={callBackFunc} />
             <MapLevelSelectWrapper>
-                <MapGrid>{getRows(rowContainer)}</MapGrid>
+                <div>
+                    <MapGrid>{getRows(rowContainer)}</MapGrid>
+                </div>
+                <MapLegend>
+                    About the different types of levels
+                    <ul>
+                        {levelTypes.map((lvlType) => (
+                            <li
+                                key={lvlType.name}
+                                onMouseOver={() =>
+                                    setHighlightedLevelType(lvlType.name)
+                                }
+                                onMouseLeave={() => setHighlightedLevelType('')}
+                            >
+                                {lvlType.name}
+                            </li>
+                        ))}
+                    </ul>
+                </MapLegend>
             </MapLevelSelectWrapper>
             <div ref={divRef}>Select a starting point</div>
         </>
